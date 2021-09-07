44af594f0db7d68f095d774346a72f5244521346
nikoada <nikushac@gmail.com>
2021-09-07 21:22:26 +0200

    feat: add UseEffectComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import UseStateComponent from "./UseStateComponent";
+import UseEffectComponent from "./UseEffectComponent";
 
 function App() {
   return (
     <div>
+      <h1>useEffect</h1>
+      <UseEffectComponent />
       <h1>useState</h1>
       <UseStateComponent />
     </div>
--- src/UseStateComponent.tsx
+++ src/UseStateComponent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function UseStateComponent() {
     const [arr, setArr] = useState<number[]>([]);
