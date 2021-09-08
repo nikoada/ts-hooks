a7959b5ffed5557e1dde025274bbc3d31f66d797
nikoada <nikushac@gmail.com>
2021-09-08 23:37:15 +0200

    feat: add UseContextComponent

---
+++ src/UseContextComponent.tsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from 'react';
+
+import UserContext, { UserState } from './store';
+
+function ConsumerComponent() {
+    const user = useContext<UserState>(UserContext);
+
+    return (
+        <div>
+            <div>First: {user.first}</div>
+            <div>Last: {user.last}</div>
+        </div>
+    )
+}
+
+function UseContextComponent() {
+    const [user, setUser] = useState<UserState>({
+        first: "Jane",
+        last: "Smith"
+    });
+    return (
+        <UserContext.Provider value={user}>
+            <ConsumerComponent />
+            <button
+                onClick={() => setUser({
+                    first: "Josie",
+                    last: "Wales"
+                })}>Change user</button>
+        </UserContext.Provider>
+    )
+    
+}
+
+export default UseContextComponent;
