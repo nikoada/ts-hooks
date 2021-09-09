32466a57016191c397e62ed42a862477e35dd8c0
nikoada <nikushac@gmail.com>
2021-09-09 23:27:16 +0200

    feat: add UseReducerComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import UseStateComponent from "./UseStateComponent";
 import UseEffectComponent from "./UseEffectComponent";
 import UseContextComponent from "./UseContextComponent";
+import UseReducerComponent from "./UseReducerComponent";
 
 function App() {
   return (
     <div>
+      <h1>useReducer</h1>
+      <UseReducerComponent />
       <h1>useContext</h1>
       <UseContextComponent />
       <h1>useEffect</h1>
